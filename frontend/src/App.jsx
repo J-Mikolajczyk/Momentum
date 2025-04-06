b0e638a5c2c00cc7007a6fdefcebf82df253ecee
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2025-04-05 20:19:36 -0400

    Adding a very basic login form after splash
    This needs to change it's so ugly and also might want to just separate register and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react';
 import SplashScreen from './components/SplashScreen';
+import LoginRegister from './components/LoginRegister'; // import LoginRegister
 
 function App() {
+  const [showLoginRegister, setShowLoginRegister] = useState(false);
+
+  const handleShowLoginRegister = () => {
+    setShowLoginRegister(true); 
+  };
+
   return (
-    <>
-      <SplashScreen />
-    </>
+    <div>
+      {showLoginRegister ? (
+        <LoginRegister />
+      ) : (
+        <SplashScreen onClick={handleShowLoginRegister} />
+      )}
+    </div>
   );
 }
 
