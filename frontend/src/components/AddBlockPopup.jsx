0ccb9ddcda4d83e2267128598649f004b56ff415
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2025-04-17 11:17:14 -0400

    Creating Home screen with block and username
    closes #1

--- frontend/src/components/AddBlockPopup.jsx
+++ frontend/src/components/AddBlockPopup.jsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 import { postRequest } from '../utils/api';
 
 export default function AddBlockPopup( {open, toggleAddBlockMenu, userInfo, setUserInfo} ) {
-    const [blockName, setBlockName] = useState(null);
+    const [blockName, setBlockName] = useState('');
+    const [message, setMessage] = useState('');
 
     const userId = userInfo?.id ? userInfo.id.toString() : null;
     const email = userInfo?.email != null ? userInfo.email : null;
-
-    console.log(userInfo)
 
     const handleSubmit = async (e) => {
       e.preventDefault();
@@ -16,25 +15,29 @@ export default function AddBlockPopup( {open, toggleAddBlockMenu, userInfo, setU
               const response = await postRequest('http://localhost:8080/block/create', { blockName, userId });
               if (response.ok) {
                 try {
-                  const response = await postRequest('http://localhost:8080/user/refresh', { email });
-                  if(response.ok) {
-                    const json = await response.json();
+                  const refreshResponse = await postRequest('http://localhost:8080/user/refresh', { email });
+                  if(refreshResponse.ok) {
+                    const json = await refreshResponse.json();
                     setUserInfo(json);
+                    setMessage('')
+                    setBlockName('');
                   } else {
-                    console.log('not ok')
-                    return (<><p>Not Ok</p></>);
+                    setMessage('Error refreshing user info')
+                    return null;
                   }
                 } catch (err) {
-                  console.log(err)
-                  return (<><p>Server error</p></>);
+                  setMessage(err)
+                  return null;
                 }
                 toggleAddBlockMenu();
-                return (<><p>OK</p></>);
+                return null;
               } else {
-                return (<><p>Not Ok</p></>);
+                setMessage('Error creating block')
+                return null;
               }
             } catch (err) {
-              return (<><p>Server error</p></>);
+              setMessage('Error creating block')
+              return null;
             }
     }
 
@@ -45,13 +48,15 @@ export default function AddBlockPopup( {open, toggleAddBlockMenu, userInfo, setU
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-sm">
-          <div className="bg-white p-6 rounded-xl shadow-lg w-3/4 h-1/5">
+          <div className="bg-white p-6 rounded-xl shadow-lg w-3/4 h-1/4">
             <p className='inline text-blue-900 font-anton text-2xl'>Add a training block</p>
             <button onClick={toggleAddBlockMenu} className="font-anton inline ml-8 text-gray-500 hover:text-gray-700 text-2xl ">X</button>
             <form>
                 <input className='mt-3 bg-white font-anton rounded-md text-blue-900 h-1/4 w-6/7 pl-3 text-2xl border-blue-900 border-2' placeholder='Block Name' value={blockName} onChange={(e) => setBlockName(e.target.value)} required /> 
                 <button type='submit' onClick={handleSubmit} className='mt-3 bg-white font-anton rounded-md text-blue-900 hover:bg-gray-200 transition duration-300 h-1/4 px-10 text-2xl border-blue-900 border-2'>Add</button>
+                {message && (<p className='font-anton text-red-700 text-xl mt-2'>{message}</p> )}
             </form>
+            
           </div>
         </div>
       );
