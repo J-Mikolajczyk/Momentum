1a8d8fc9dfc4a64a0b66708af5eae3efa8d42e2e
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2025-04-19 08:45:09 -0400

    Cleaning up closeAddBlock and keeping nav above sidebar

--- frontend/src/components/AddBlockPopup.jsx
+++ frontend/src/components/AddBlockPopup.jsx
@@ -8,7 +8,25 @@ export default function AddBlockPopup( {open, toggleAddBlockMenu, userInfo, setU
     const userId = userInfo?.id ? userInfo.id.toString() : null;
     const email = userInfo?.email != null ? userInfo.email : null;
 
+    const handleClose = () => {
+      toggleAddBlockMenu();
+      setBlockName('');
+      setMessage('');
+    }
+
+    const handleOuterClick = () => {
+      handleClose();
+    };
+  
+    const handleInnerClick = (e) => {
+      e.stopPropagation();
+    };
+
     const handleSubmit = async (e) => {
+      if (blockName === '' || blockName === null) {
+        setMessage('Block Name is required.')
+        return null;
+      }
       e.preventDefault();
 
       try {
@@ -19,8 +37,7 @@ export default function AddBlockPopup( {open, toggleAddBlockMenu, userInfo, setU
                   if(refreshResponse.ok) {
                     const json = await refreshResponse.json();
                     setUserInfo(json);
-                    setMessage('')
-                    setBlockName('');
+                    handleClose();
                   } else {
                     setMessage('Error refreshing user info')
                     return null;
@@ -43,16 +60,18 @@ export default function AddBlockPopup( {open, toggleAddBlockMenu, userInfo, setU
 
 
     if (!open) {
-        return null;
+        return <></>;
     }
 
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-sm">
-          <div className="bg-white p-6 rounded-xl shadow-lg w-3/4 h-1/4">
+        <div onClick={handleOuterClick} className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-sm">
+          <div onClick={handleInnerClick} className="bg-white p-5 pr-7 rounded-xl shadow-lg w-3/4 h-1/5">
+            <div className='flex justify-between'>
             <p className='inline text-blue-900 font-anton text-2xl'>Add a training block</p>
-            <button onClick={toggleAddBlockMenu} className="font-anton inline ml-8 text-gray-500 hover:text-gray-700 text-2xl ">X</button>
+            <button onClick={handleClose} className="font-anton inlinetext-gray-500 hover:text-gray-700 text-2xl ">X</button>
+            </div>
             <form>
-                <input className='mt-3 bg-white font-anton rounded-md text-blue-900 h-1/4 w-6/7 pl-3 text-2xl border-blue-900 border-2' placeholder='Block Name' value={blockName} onChange={(e) => setBlockName(e.target.value)} required /> 
+                <input className='mt-3 bg-white font-anton rounded-md text-blue-900 h-1/4 w-6/7 text-2xl border-blue-900 border-2' placeholder='Block Name' value={blockName} onChange={(e) => setBlockName(e.target.value)} required /> 
                 <button type='submit' onClick={handleSubmit} className='mt-3 bg-white font-anton rounded-md text-blue-900 hover:bg-gray-200 transition duration-300 h-1/4 px-10 text-2xl border-blue-900 border-2'>Add</button>
                 {message && (<p className='font-anton text-red-700 text-xl mt-2'>{message}</p> )}
             </form>
