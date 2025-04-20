6e2b1cbb7f363d449b489bd4c9d0b2856812d2f4
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2025-04-19 23:11:30 -0400

    Creating basic block component
    Closes #39

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import AddBlockPopup from '../components/AddBlockPopup';
 import Sidebar from '../components/Sidebar'
+import Block from '../components/Block'
 
 function Home({ userInfo, setUserInfo }) {
   const [showSidebar, setShowSidebar] = useState(false);
   const [showAddBlockMenu, setshowAddBlockMenu] = useState(false);
+  const [blockName, setBlockName] = useState(null);
 
   const name = userInfo?.name != null ? userInfo.name : null;
 
@@ -16,6 +18,10 @@ function Home({ userInfo, setUserInfo }) {
     setShowSidebar(!showSidebar);
   }
 
+  const openBlock = (blockName) => {
+    setBlockName(blockName);
+  }
+
   return (
     <div className='h-screen flex flex-col bg-white'>
       <nav className='h-1/12 flex shrink-0 justify-between items-center bg-blue-800 pl-2.5'>
@@ -23,18 +29,25 @@ function Home({ userInfo, setUserInfo }) {
         <button onClick={toggleSidebar} className='select-none text-white font-anton text-6xl pb-2 w-1/6'>≡</button>
       </nav>
       <Sidebar open={showSidebar} toggleSidebar={toggleSidebar} userInfo={userInfo} setUserInfo={setUserInfo}/>
+      <div className='flex flex-col h-screen items-center pt-3 mx-6 gap-2'>
+      { blockName === null ? 
+        (<>
+          <div className='flex w-full items-center mb-3'>
+            <p className='text-blue-800 font-anton inline-block text-3xl'>Welcome, {name}</p>
+            <button onClick={toggleAddBlockMenu} className='inline-block elect-none bg-gray-400 text-gray-500 font-anton ml-auto w-1/4 h-10 text-xl border border-gray-500' > Add Block </button>
+          </div>
+          {userInfo?.trainingBlockNames?.length > 0 ? (
+          userInfo.trainingBlockNames.map((blockName, index) => (
+          <button onClick={() => openBlock(blockName)} key={index} className='bg-blue-50 text-blue-800 font-anton px-4 py-2 rounded-md shadow-md w-full text-left text-2xl border-blue-800 border-1'>{blockName}</button> ))
+          ) : (<p className='text-gray-500 font-anton text-2xl'>No Training Blocks Found</p>)}         
+        </>) : (<>
+          <Block blockName={blockName}/>
+        </>)
+      }
+      </div> 
       <AddBlockPopup open={showAddBlockMenu} toggleAddBlockMenu={toggleAddBlockMenu} userInfo={userInfo} setUserInfo={setUserInfo}/>
 
-      <div className='flex flex-col h-screen items-center pt-3 mx-6 gap-2'>
-        <div className='flex w-full items-center mb-3'>
-          <p className='text-blue-800 font-anton inline-block text-3xl'>Welcome, {name}</p>
-          <button onClick={toggleAddBlockMenu} className='inline-block elect-none bg-gray-400 text-gray-500 font-anton ml-auto w-1/4 h-10 text-xl border border-gray-500' > Add Block </button>
-        </div>
-        {userInfo?.trainingBlockNames?.length > 0 ? (
-          userInfo.trainingBlockNames.map((blockName, index) => (
-          <button key={index} className='bg-blue-50 text-blue-800 font-anton px-4 py-2 rounded-md shadow-md w-full text-left text-2xl border-blue-800 border-1'>{blockName}</button> ))
-        ) : (<p className='text-gray-500 font-anton text-2xl'>No Training Blocks Found</p>)}
-      </div>
+      
     </div>
   );
 }
