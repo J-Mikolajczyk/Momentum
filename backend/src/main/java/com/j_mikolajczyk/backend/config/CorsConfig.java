be43fc34c029a9fe54aea0c51f1bf2beba509f1f
John Mikolajczyk <johnpaulmik@gmail.com>
2025-04-21 06:33:04 -0400

    Adding frontend url to allowed Origins

--- backend/src/main/java/com/j_mikolajczyk/backend/config/CorsConfig.java
+++ backend/src/main/java/com/j_mikolajczyk/backend/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedOrigins("https://momentumtraining.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
