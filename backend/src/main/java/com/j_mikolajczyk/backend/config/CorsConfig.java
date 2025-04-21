cbcc98791f5ae9f4cec706ddc7dd9610048f94cc
John Mikolajczyk <johnpaulmik@gmail.com>
2025-04-21 08:25:01 -0400

    Update CorsConfig.java

--- backend/src/main/java/com/j_mikolajczyk/backend/config/CorsConfig.java
+++ backend/src/main/java/com/j_mikolajczyk/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","https://momentumtraining.netlify.app")
+                        .allowedOrigins("http://localhost:5173","https://momentumtraining.netlify.app","https://training-momentum.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
