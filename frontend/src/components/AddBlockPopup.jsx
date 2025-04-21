e954a4f0255e9784bc9b0ca2860514de4dbac648
John Mikolajczyk <johnpaulmik@gmail.com>
2025-04-21 08:31:12 -0400

    Update AddBlockPopup.jsx

--- frontend/src/components/AddBlockPopup.jsx
+++ frontend/src/components/AddBlockPopup.jsx
@@ -66,7 +66,7 @@ export default function AddBlockPopup( {open, toggleAddBlockMenu, userInfo, setU
 
     return (
         <div onClick={handleOuterClick} className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 backdrop-blur-sm">
-          <div onClick={handleInnerClick} className="bg-white p-5 pr-7 rounded-xl shadow-lg w-3/4 h-1/5">
+          <div onClick={handleInnerClick} className="bg-white p-5 pr-7 rounded-xl shadow-lg w-3/4 h-1/4">
             <div className='flex justify-between'>
             <p className='inline text-blue-900 font-anton text-2xl'>Add a training block</p>
             <button onClick={handleClose} className="font-anton inlinetext-gray-500 hover:text-gray-700 text-2xl ">X</button>
