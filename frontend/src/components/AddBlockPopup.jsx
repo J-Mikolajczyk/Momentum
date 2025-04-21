3cb180c48de57b1d21cbf4b1f6160298cafa66c9
John Mikolajczyk <johnpaulmik@gmail.com>
2025-04-21 06:43:22 -0400

    Removing 8080 to fix Render issue

--- frontend/src/components/AddBlockPopup.jsx
+++ frontend/src/components/AddBlockPopup.jsx
@@ -31,10 +31,10 @@ export default function AddBlockPopup( {open, toggleAddBlockMenu, userInfo, setU
       e.preventDefault();
 
       try {
-              const response = await postRequest('https://'+ip+':8080/block/create', { blockName, userId });
+              const response = await postRequest('https://'+ip+'/block/create', { blockName, userId });
               if (response.ok) {
                 try {
-                  const refreshResponse = await postRequest('https://'+ip+':8080/user/refresh', { email });
+                  const refreshResponse = await postRequest('https://'+ip+'/user/refresh', { email });
                   if(refreshResponse.ok) {
                     const json = await refreshResponse.json();
                     setUserInfo(json);
