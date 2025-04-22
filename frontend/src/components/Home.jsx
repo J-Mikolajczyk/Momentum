7971411f089eab59de91f783e7098b53d91db8d5
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2025-04-22 19:46:24 -0400

    Updating user-select and nav bar

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -14,6 +14,10 @@ function Home({ userInfo, setUserInfo }) {
     setshowAddBlockMenu(!showAddBlockMenu);
   };
 
+  const goHome = () => {
+    setBlockName(null);
+  }
+
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
   }
@@ -25,7 +29,7 @@ function Home({ userInfo, setUserInfo }) {
   return (
     <div className='h-screen flex flex-col bg-white'>
       <nav className='h-1/12 flex shrink-0 justify-between items-center bg-blue-800 pl-2.5'>
-        <p className='select-none text-white font-anton text-5xl'>MOMENTUM</p>
+        <button onClick={goHome} className='select-none text-white font-anton text-5xl'>MOMENTUM</button>
         <button onClick={toggleSidebar} className='select-none text-white font-anton text-6xl pb-2 w-1/6'>≡</button>
       </nav>
       <Sidebar open={showSidebar} toggleSidebar={toggleSidebar} userInfo={userInfo} setUserInfo={setUserInfo}/>
