46cc71566a5fa2cfa45fcba3aebfaeb4c595892f
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2025-04-24 09:58:31 -0400

    dynamic island fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div className='min-h-full'>
       { loggedIn ? 
         (<><Home userInfo={userInfo} setUserInfo={setUserInfo}/></>)
         :(<> { showEmailForm ?
