d3b37fcd7c19c70a9f18f67a1a13506d8691566e
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2025-05-01 00:04:34 -0400

    Adding logout button to sidebar
    Closes #36

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { postRequest } from '../utils/api';
 
-export default function Sidebar( {open, toggleSidebar, userInfo, setUserInfo} ) {
+export default function Sidebar( { logOut, open, toggleSidebar, userInfo, setUserInfo} ) {
 
     const handleOuterClick = () => {
       toggleSidebar();
@@ -19,7 +19,8 @@ export default function Sidebar( {open, toggleSidebar, userInfo, setUserInfo} ) 
     return (
         <div className="fixed bottom-0 left-0 right-0 z-50 bg-black/50 flex items-center justify-end bg-black/30 h-11/12" onClick={handleOuterClick}>
           <div className="bg-white p-6 shadow-lg w-5/6 h-full" onClick={handleInnerClick}>
-            
+              <button onClick={logOut} className='inline-block elect-none bg-gray-400 text-gray-500 font-anton ml-auto w-1/4 min-w-21 h-10 text-xl border border-gray-500 rounded-xs' >Log Out</button>
+        
           </div>
         </div>
       );
