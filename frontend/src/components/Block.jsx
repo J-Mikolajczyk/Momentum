4c7726d5f9e8e551effa163405a080f8184fb0b3
John Mikolajczyk <124541592+J-Mikolajczyk@users.noreply.github.com>
2025-05-11 21:01:38 -0400

    Update Block.jsx

--- frontend/src/components/Block.jsx
+++ frontend/src/components/Block.jsx
@@ -9,7 +9,7 @@ export default function Block({ currentWeekIndex, currentDayIndex, blockData, ad
 
   return (
     <>
-      <div className="flex flex-row w-full flex-grow items-start gap-4 px-4 pt-4 overflow-y-auto scrollbar-hide" style={{ maxHeight: 'calc(100dvh - 70px)' }}>
+      <div className="flex flex-row w-full flex-grow items-start gap-4 px-4 pt-4 overflow-y-auto scrollbar-hide" style={{ maxHeight: 'calc(100dvh - 130px)' }}>
         <div className="flex flex-col items-center rd w-full">
           <Day
               blockData={blockData}
