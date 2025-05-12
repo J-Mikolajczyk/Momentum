7b1ddcf85f84a3e939e0d49a52a381269cbf79e5
John Mikolajczyk <johnpaulmik@gmail.com>
2025-05-12 10:11:46 -0400

    Update Day.jsx

--- frontend/src/components/Day.jsx
+++ frontend/src/components/Day.jsx
@@ -31,7 +31,7 @@ export default function Day({
 
 
   return (
-    <div className="flex flex-col w-full flex-grow items-start gap-4 px-4 pt-4 overflow-y-auto scrollbar-hide" style={{ maxHeight: 'calc(100dvh - 140px)' }}>
+    <div className="flex flex-col w-full flex-grow items-start gap-4 px-4 pt-4 overflow-y-auto scrollbar-hide" style={{ maxHeight: 'calc(100dvh - 160px)' }}>
           
         
       {exercises?.length > 0 ? (
