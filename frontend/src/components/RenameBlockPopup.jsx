d21ee9e68fe62ae3529d04368713b6f72d922dd9
John Mikolajczyk <johnpaulmik@gmail.com>
2025-05-15 06:55:36 -0400

    Adding block renaming
    Closes #112

---
+++ frontend/src/components/RenameBlockPopup.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import setThemeColor from '../hooks/useThemeColor'
+
+export default function RenamePopup( { show, toggle, name, rename } ) {
+
+    if (!show) {
+        return <></>;
+    }
+
+    const [newName, setNewName] = useState('');
+
+    useEffect(() => {
+        setThemeColor('#0D1E5C'); 
+    }, []);
+
+    const handleClose = () => {
+        toggle();
+        setThemeColor('#193cb8')
+    }
+
+    const handleOuterClick = () => {
+      handleClose();
+    };
+  
+    const handleInnerClick = (e) => {
+      e.stopPropagation();
+    };
+
+    if (!open) {
+        return <></>;
+    }
+
+    return (
+        <div onClick={handleOuterClick} className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+          <div onClick={handleInnerClick} className="bg-white p-5 pr-7 rounded-xl shadow-lg w-3/4 min-h-1/5">
+            <div className='flex justify-between'>
+            <p className='inline text-blue-900 font-anton text-2xl'>Rename Block</p>
+            <button onClick={handleClose} className="font-anton inlinetext-gray-500 hover:text-gray-700 text-2xl ">X</button>
+            </div>
+            <form>
+                <input className='mt-3 bg-white font-anton rounded-md text-blue-900 h-1/4 w-full text-2xl border-blue-900 border-2 pl-3' placeholder={name} value={newName} onChange={(e) => setNewName(e.target.value)} required /> 
+                <button type='submit' onClick={() => {handleClose(), rename(name, newName)}} className='mt-3 bg-white font-anton rounded-md text-blue-900 hover:bg-gray-200 transition duration-300 h-1/4 px-10 text-2xl border-blue-900 border-2'>Rename</button>
+            </form>
+            
+          </div>
+        </div>
+      );
+      
+}
