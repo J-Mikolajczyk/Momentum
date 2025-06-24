9b09415eb9c6358cac7febe9e5a3555e98c07373
John Mikolajczyk <johnpaulmik@gmail.com>
2025-06-24 08:56:31 -0400

    Starting auth testing

---
+++ backend/src/test/java/com/j_mikolajczyk/backend/controllers/TestAuthController.java
@@ -0,0 +1,177 @@
+package com.j_mikolajczyk.backend.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.j_mikolajczyk.backend.dto.UserDTO;
+import com.j_mikolajczyk.backend.exceptions.UserAlreadyExistsException;
+import com.j_mikolajczyk.backend.models.TrainingBlock;
+import com.j_mikolajczyk.backend.models.User;
+import com.j_mikolajczyk.backend.requests.user.LoginRequest;
+import com.j_mikolajczyk.backend.requests.user.RegisterRequest;
+import com.j_mikolajczyk.backend.services.TrainingBlockService;
+import com.j_mikolajczyk.backend.services.UserService;
+import com.j_mikolajczyk.backend.utils.AuthGuard;
+import com.j_mikolajczyk.backend.utils.CookieUtil;
+import com.j_mikolajczyk.backend.utils.JwtUtil;
+import com.j_mikolajczyk.backend.repositories.TrainingBlockRepository;
+
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.naming.NameNotFoundException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@WebMvcTest(controllers = AuthController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+public class TestAuthController {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TrainingBlockService blockService;
+
+    @MockBean
+    private TrainingBlockRepository blockRepository;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private JwtUtil jwtUtil;
+
+    @MockBean
+    private CookieUtil cookieUtil;
+
+    @MockBean
+    private AuthGuard authGuard;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String email;
+    private String password;
+    private String name;
+    private User user;
+    private UserDTO userDTO;
+
+    @BeforeEach
+    void setUp() {
+        email = "email";
+        password = "password";
+        name = "name";
+        user = new User(name, email, password);
+        user.setId(new ObjectId());
+        userDTO = new UserDTO(user);
+    }
+
+    @Test
+    void testRegisterSuccess() throws Exception {
+        RegisterRequest request = new RegisterRequest(email, password, name);
+
+        doNothing().when(userService).register(request);
+
+        mockMvc.perform(post("/auth/register")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isCreated());
+    }
+
+    @Test
+    void testRegisterConflict() throws Exception {
+        RegisterRequest request = new RegisterRequest(email, password, name);
+
+        doThrow(new UserAlreadyExistsException("409")).when(userService).register(any(RegisterRequest.class));
+
+        mockMvc.perform(post("/auth/register")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isConflict());
+    }
+
+    @Test
+    void testRegisterException() throws Exception {
+        RegisterRequest request = new RegisterRequest(email, password, name);
+
+        doThrow(new Exception()).when(userService).register(any(RegisterRequest.class));
+
+        mockMvc.perform(post("/auth/register")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testLoginSuccess() throws Exception {
+        LoginRequest request = new LoginRequest(email, password);
+
+        when(userService.login(any(LoginRequest.class)))
+            .thenReturn(userDTO);
+        when(jwtUtil.generateTokens(any(UserDTO.class)))
+            .thenReturn(Map.of("longTermToken", "longTermToken", "shortTermToken", "shortTermToken"));
+        when(cookieUtil.createCookie(anyString(), anyString(), anyInt(), anyString()))
+            .thenReturn(new Cookie("longTermCookie", "longTermToken"));
+        
+        mockMvc.perform(post("/auth/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    void testLoginNotFound() throws Exception {
+        LoginRequest request = new LoginRequest(email, password);
+
+        when(userService.login(any(LoginRequest.class)))
+            .thenThrow(new NotFoundException());
+        
+        mockMvc.perform(post("/auth/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andExpect(content().string("{\"exists\": false}"));
+    }
+
+    @Test
+    void testLoginBadCredentials() throws Exception {
+        LoginRequest request = new LoginRequest(email, password);
+
+        when(userService.login(any(LoginRequest.class)))
+            .thenThrow(new BadCredentialsException("Bad credentials"));
+        
+        mockMvc.perform(post("/auth/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isUnauthorized());
+    }
+
+
+}
+
+    
+
